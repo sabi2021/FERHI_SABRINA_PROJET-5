d20b2dac1dcd3295f6bd04e5271385bec232f965
sabi2021 <ferhi.sabrina97@gmail.com>
2023-03-22 11:37:12 +0100

    ajout du fichier module cart.js

---
+++ front/js/modules/cart.js
@@ -0,0 +1,125 @@
+import {
+    getOneProduct
+} from "./api.js";
+
+const loadCart = function() {
+    const arrayCart = JSON.parse(localStorage.getItem("cartLS"));
+
+    // if (!arrayCart) {
+    //     return [];
+    // } else {
+    //     return arrayCart;v 
+    // }
+
+    /**
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+     */
+    // return !arrayCart ? [] : arrayCart;
+
+    /**
+     * @seehttps://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/Logical_OR
+     */
+    // return arrayCart || [];
+
+    /**
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
+     */
+    return arrayCart ?? [];
+}
+
+const saveCart = function() {
+    localStorage.setItem("cartLS", JSON.stringify(cart)); 
+}
+
+const findProductInCart = function(id, color) {
+    return cart.find(item => item.productId === id && item.color === color);
+}
+
+const checkCartRules = function (productId, color, quantity) {
+    if (!productId) {
+        throw new Error("L'identifiant produit n'est pas valide");
+    }
+
+    if (!color) {
+        throw new Error("Pas de couleur sélectionnée");
+    }
+
+    if (!quantity || parseInt(quantity, 10) <= 0) {
+        throw new Error("La quantité n'est pas valide");
+    }
+}
+
+const addProductToCart = function(productId, color, quantity) {
+    checkCartRules(productId, color, quantity);
+
+    const cartItem = findProductInCart(productId, color);
+
+    if (!cartItem) {
+        cart.push({
+            productId,
+            color,
+            quantity: parseInt(quantity, 10)
+        });
+    } else {
+        cartItem.quantity += Number(quantity);
+    }
+
+    saveCart();
+}
+
+const updateProductQuantityInCart = function(productId, color, quantity) {
+    
+    checkCartRules(productId, color, quantity);
+
+    const cartItem = findProductInCart(productId, color);
+
+    if (!cartItem) {
+        throw new Error('Il faut que le produit existe dans le panier pour pouvoir changer sa quantité');
+    }
+
+    cartItem.quantity = Number(quantity);
+
+    saveCart();
+}
+
+const removeProductFromCart = function(id, color) {
+    const indexOfItemToDelete = cart.findIndex((element) => {
+        return (
+            element.productId === id &&
+            element.color === color
+        );
+    });
+
+    if (indexOfItemToDelete === -1) {
+        throw new Error("Le produit ne peut pas être supprimé car il n'existe pas dans le panier");
+    }
+
+    cart.splice(indexOfItemToDelete, 1); 
+    saveCart();
+}
+
+const getCartContent = async function() {
+    const items = Array.from(cart);
+    let total = Number(0);
+
+    for(const index in items){
+        const item = items[index];
+        const { name, price, imageUrl, altTxt } = await getOneProduct(item.productId);
+
+        items[index] = {
+            ...item,
+            name,
+            price,
+            imageUrl,
+            altTxt,
+        };
+
+        total += Number(item.quantity) * price;
+    }
+
+    return { items, total };
+}
+
+const cart = loadCart();
+
+export  { addProductToCart, updateProductQuantityInCart, removeProductFromCart, getCartContent }
